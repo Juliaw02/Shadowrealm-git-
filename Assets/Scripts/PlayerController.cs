217ff4bbd61c70ac23ca7a938cf9f334afc49260
Juliaw02 <jwill309@depaul.edu>
2022-04-19 20:49:44 -0500

    Some basic player stuff
    Started basic input stuff for the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rbody;
+    public Rigidbody2D rigidbody;
+    public float playerSpeed;
+    public float jumpSpeed;
+
+    private bool grounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // A + D or left and right arrow keys to move left and right
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(horizontalInput * playerSpeed, rigidbody.velocity.y);
+
+        // Flip player in the direction they're moving in
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(0.5f, 1, 1);
+        } else if (horizontalInput < -0.0f)
+        {
+            transform.localScale = new Vector3(-0.5f, 1, 1);
+        }
+
+        // Space to jump
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpSpeed);
+        grounded = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
     }
 }
